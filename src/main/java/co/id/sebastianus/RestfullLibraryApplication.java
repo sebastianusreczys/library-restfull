fc620c6fc902f27044d1f2172aa296b528d6093d
sebastianusreczys <sebastianusreczy17@gmail.com>
2024-04-28 11:51:21 +0700

    setup project

---
+++ src/main/java/co/id/sebastianus/RestfullLibraryApplication.java
@@ -0,0 +1,13 @@
+package co.id.sebastianus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/sebastianus/RestfullLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.sebastianus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
